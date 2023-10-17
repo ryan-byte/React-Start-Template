31404954c848dc06c6f01245bc159b1d3311a957
ryan-byte <thesoulsreaper15@gmail.com>
2023-10-17 15:05:49 +0100

    React app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+    console.log("test");
+    return (
+        <h1>
+            Welcome to my React App!
+        </h1>
+    )
+}
+
+export default App
